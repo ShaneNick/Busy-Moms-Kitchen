fd975805f2e6eb593b20a6d2abeb7b1c664d2762
ShaneNick <shanetomonia@gmail.com>
2023-06-01 12:14:59 -0400

    new js css

--- public/js/model.js
+++ public/js/model.js
@@ -1,10 +1,10 @@
-export async function getMealList(searchInputTxt) {
+ async function getMealList(searchInputTxt) {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`);
     const data = await response.json();
     return data.meals;
   }
   
-  export async function fetchMealDetails(mealId) {
+   async function fetchMealDetails(mealId) {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`);
     const data = await response.json();
     return data.meals[0];
